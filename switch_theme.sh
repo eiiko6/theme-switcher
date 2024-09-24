2fc46658e36a9aafee044d8acb53ca4e20f5a092
eiiko6 <mxstotolatomate@gmail.com>
2024-09-24 16:48:25 +0200

    Removed theme cycle option to focus on the -s option, moved config files to the user's config directory, added wal colorscheme creation

--- switch_theme.sh
+++ switch_theme.sh
@@ -7,6 +7,7 @@ update_files() {
   ln -sf $1/kitty/kitty.conf ~/.config/kitty/kitty.conf
   ln -sf $1/wofi/style.css ~/.config/wofi/style.css
   ln -sf $1/hypr/appearance.conf ~/.config/hypr/appearance.conf
+  ln -sf $1/hypr/animations.conf ~/.config/hypr/animations.conf
   ln -sf $1/random-krabby/pokemon-list.conf ~/.config/random-krabby/pokemon-list.conf
   ln -sf $1/nvim/lua/plugins/colorscheme.lua ~/.config/nvim/lua/plugins/colorscheme.lua
 
@@ -16,7 +17,6 @@ update_files() {
   # Restart waybar
   pkill waybar
   nohup waybar >/dev/null 2>&1 &
-  echo "> Switched theme to $1"
 }
 
 # Function to switch themes
@@ -50,51 +50,39 @@ switch_theme() {
   echo $next_theme_num >./next_theme_num
 
   update_files $theme_config_dir $theme_wallpaper $theme_name
+  echo "Updated config files"
+
+  # generate the colors using wal
+  wal -i $theme_wallpaper -n -q -t --saturate 0.5 2>/dev/null
+  # set the background opacity of kitty
+  sed -i '3s/.*/background_opacity 0.6/' ~/.cache/wal/colors-kitty.conf
+  # set the wallpaper using swww
+  swww img $theme_wallpaper -t none
+  # change the fastfetch logo
+  magick -size 500x500 xc:none -draw "roundrectangle 0,0,500,500,32,32" ~/.config/fastfetch/mask.png 2>/dev/null
+  magick $theme_wallpaper -resize 500x500^ -gravity center -extent 500x500 -alpha set ~/.config/fastfetch/mask.png -compose DstIn -composite /home/mxstoto/.config/fastfetch/fetch-logo.png 2>/dev/null
+  echo "Applied wallpaper and colorscheme"
 }
 
-# Function to switch themes
-switch_theme_num() {
-  local theme_num=$1
-  local theme_wallpaper=""
-  local theme_config_dir=""
-
-  # Find the theme details from the config file
-  eval current_theme_name=\$THEME${theme_num}_NAME
-  eval current_theme_wallpaper=\$WALLPAPER_NAME
-  eval current_theme_config_dir=\$THEME${theme_num}_CONFIG_DIR
-
-  if [[ -z "$current_theme_name" || -z "$current_theme_wallpaper" || -z "$current_theme_config_dir" ]]; then
-    echo "Theme '$theme_num' not found in configuration."
-    exit 1
-  fi
-
-  theme_config_dir=$current_theme_config_dir
-  theme_wallpaper="${current_theme_config_dir}/${current_theme_wallpaper}"
-  echo $theme_wallpaper
-
-  # Update the current theme file with the next theme number
-  next_theme_num=$((theme_num % NUM_THEMES + 1))
-  echo $next_theme_num >./next_theme_num
-
-  update_files $theme_config_dir $theme_wallpaper $theme_name
-}
+# Create config files if they don't exist
+if [ -d "$HOME/.config/themeSwitcher" ]; then
+  echo ""
+else
+  echo "Creating themeSwitcher config files..."
+  cp -r ./config/themeSwitcher ~/.config/
+fi
 
 # Load the configuration file
-source ./themes.conf
+source ~/.config/themeSwitcher/themes.conf
 
 # Check if any parameter is provided
 if [[ $# -eq 0 ]]; then
-  echo "Usage: $0 [--list(-l)] | [--cycle(-c)] | [--select(-s) <theme_name>]"
+  echo "Usage: $0 [--list(-l)] | [--select(-s) <theme_name>]"
   exit 1
 fi
 
 # Handle command-line parameters
 case $1 in
--c | --cycle)
-  # Get the current theme number from the file
-  next_theme_num=$(<./next_theme_num)
-  switch_theme_num "$next_theme_num"
-  ;;
 -s | --select)
   # Select a specific theme by number
   if [[ $# -ne 2 ]]; then
@@ -102,6 +90,7 @@ case $1 in
     exit 1
   fi
   switch_theme "$2"
+  echo "> Switched theme to $2"
   ;;
 -l | --list)
   # List all themes
@@ -109,7 +98,7 @@ case $1 in
   ;;
 *)
   echo "Invalid option: $1"
-  echo "Usage: $0 [--list(-l)] | [--cycle(-c)] | [--select(-s) <theme_name>]"
+  echo "Usage: $0 [--list(-l)] | [--select(-s) <theme_name>]"
   exit 1
   ;;
 esac
