8e6ba3dfac1a9c6ca0c1c058f3a942137483eae9
eiiko6 <mxstotolatomate@gmail.com>
2024-11-27 19:05:14 +0100

    files for symlinks are now automatically detected

--- switch_theme.sh
+++ switch_theme.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
+# Function to create/replace symlinks
 update_files() {
   # Update symlinks
-  ln -sf $1/waybar/config.jsonc ~/.config/waybar/config.jsonc
-  ln -sf $1/waybar/style.css ~/.config/waybar/style.css
-  ln -sf $1/kitty/kitty.conf ~/.config/kitty/kitty.conf
-  ln -sf $1/wofi/style.css ~/.config/wofi/style.css
-  ln -sf $1/hypr/appearance.conf ~/.config/hypr/appearance.conf
-  ln -sf $1/hypr/animations.conf ~/.config/hypr/animations.conf
-  ln -sf $1/random-krabby/pokemon-list.conf ~/.config/random-krabby/pokemon-list.conf
-  ln -sf $1/nvim/lua/plugins/colorscheme.lua ~/.config/nvim/lua/plugins/colorscheme.lua
+  for theme in $(find "$1" -type f -printf '%P\n'); do
+    echo "file found: $1/$theme -> $HOME/.config/$theme"
+    ln -sf "$1/$theme" $HOME/.config/$theme
+  done
 
   # Set the background
   swww img $2 --transition-fps 60 --transition-type wipe --transition-duration 1
@@ -22,12 +19,12 @@ update_files() {
 # Function to switch themes
 switch_theme() {
   local theme_name=$1
-  local theme_num=0
   local theme_wallpaper=""
   local theme_config_dir=""
 
   # Find the theme details from the config file
   for i in $(seq 1 $NUM_THEMES); do
+    # get variables from config file
     eval current_theme_name=\$THEME${i}_NAME
     eval current_theme_wallpaper=\$WALLPAPER_NAME
     eval current_theme_config_dir=\$THEME${i}_CONFIG_DIR
@@ -35,30 +32,29 @@ switch_theme() {
     if [[ "$current_theme_name" == "$theme_name" ]]; then
       theme_config_dir=$current_theme_config_dir
       theme_wallpaper="${theme_config_dir}/${current_theme_wallpaper}"
-      theme_num=$i
       break
     fi
   done
 
+  # if theme does not exist in config
   if [[ -z "$theme_wallpaper" || -z "$theme_config_dir" ]]; then
     echo "Theme '$theme_name' not found in configuration."
     exit 1
   fi
-
-  # Update the current theme file with the next theme number
-  next_theme_num=$((theme_num % NUM_THEMES + 1))
-  echo $next_theme_num >./next_theme_num
 
   update_files $theme_config_dir $theme_wallpaper $theme_name
   echo "Updated config files"
 
   # generate the colors using wal
   wal -i $theme_wallpaper -n -q -t --saturate 0.5 2>/dev/null
+
   # set the background opacity of kitty
   sed -i '3s/.*/background_opacity 0.6/' ~/.cache/wal/colors-kitty.conf
+
   # change the fastfetch logo
   magick -size 500x500 xc:none -draw "roundrectangle 0,0,500,500,32,32" ~/.config/fastfetch/mask.png 2>/dev/null
   magick $theme_wallpaper -resize 500x500^ -gravity center -extent 500x500 -alpha set ~/.config/fastfetch/mask.png -compose DstIn -composite /home/mxstoto/.config/fastfetch/fetch-logo.png 2>/dev/null
+
   echo "Applied wallpaper and colorscheme"
 }
 
@@ -101,6 +97,6 @@ case $1 in
   fi
 
   switch_theme "$1"
-  echo "> Switched theme to $1"
+  echo -e "\n> Switched theme to $1"
   ;;
 esac
