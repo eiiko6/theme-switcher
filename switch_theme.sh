6bc4c2e023cfc5ae072ab09dfb56e7c8ee6e9458
eiiko6 <mxstotolatomate@gmail.com>
2024-12-28 14:15:21 +0100

    added option to specify themes directory

--- switch_theme.sh
+++ switch_theme.sh
@@ -3,11 +3,11 @@
 # Function to create/replace symlinks
 update_files() {
   local theme_dir=$1
-  for theme in $(find "$theme_dir" -type f -printf '%P\n'); do
-    ln -sf "$theme_dir/$theme" "$HOME/.config/$theme"
+  for file in $(find "$theme_dir" -type f -printf '%P\n'); do
+    ln -sf "$theme_dir/$file" "$HOME/.config/$file"
 
     if [[ "$quiet" != 1 ]]; then
-      echo "Created symlink: $theme_dir/$theme -> $HOME/.config/$theme"
+      echo "Created symlink: $theme_dir/$file -> $HOME/.config/$file"
     fi
   done
 }
@@ -71,14 +71,25 @@ fi
 source "$HOME/.config/theme-switcher/themes.conf"
 
 # Create themes directory if it doesn't exist
-if [[ ! -d "$HOME/.config/theme-switcher/themes" ]]; then
+if [[ ! -d "$THEMES_DIR" ]]; then
   echo "Creating themes directory..."
-  mkdir -p "$HOME/.config/theme-switcher/themes"
+  THEMES_DIR="$HOME/.config/theme-switcher/themes"
+  mkdir -p "$THEMES_DIR"
 fi
 
 # List themes if requested
 if [[ $# -eq 0 ]]; then
-  echo "Usage: $0 [--list(-l)] | [--select(-s) <theme_name>]"
+  echo "Usage: $0 [OPTIONS] <theme_name>"
+  echo ""
+  echo "Options:"
+  echo "  -l, --list       List all available themes in ~/.config/theme-switcher/themes"
+  echo "  -q, --quiet      Suppress output for symlink creation and command execution"
+  echo "  <theme_name>     Name of the theme to switch to"
+  echo ""
+  echo "Examples:"
+  echo "  $0 -l             # List available themes"
+  echo "  $0 -q <theme_name> # Switch to <theme_name> silently (without output)"
+  echo "  $0 <theme_name>    # Switch to <theme_name> and show output"
   exit 1
 fi
 
